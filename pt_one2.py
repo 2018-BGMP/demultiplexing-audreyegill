414da51fa3a10a7557dd63de89433be07fcf4871
audreyegill <40414986+audreyegill@users.noreply.github.com>
2018-08-14 20:15:08 -0700

    Upload pt_onei.py where i is the file number
    Four separate scripts for the four fastq files.

---
+++ pt_one2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+def phred(letter):
+    '''This takes an ascii character and returns the corresponding phred score'''
+    return ord(letter) - 33
+
+# This is an array of file titles
+titles = ('read1', 'index1', 'read2', 'index2')
+# This is an array of file paths
+#files = ('/mnt/c/Users/effyg/gdrive2/Bi622/De-multiplex/test_R1.fastq','/mnt/c/Users/effyg/gdrive2/Bi622/De-multiplex/test_R2.fastq', '/mnt/c/Users/effyg/gdrive2/Bi622/De-multiplex/test_R3.fastq', '/mnt/c/Users/effyg/gdrive2/Bi622/De-multiplex/test_R4.fastq')
+files= ('/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R1_001.fastq.gz', '/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R2_001.fastq.gz', '/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R3_001.fastq.gz', '/projects/bgmp/shared/2017_sequencing/1294_S1_L008_R4_001.fastq.gz')
+
+
+# Import packages
+import numpy as np
+
+def bp_means(file, n):
+    # create np array of 0's with name data_title
+    data= np.zeros(101)
+    # open file using file_path
+    with open(file, 'r') as f:
+        while True:
+            line1 = f.readline()
+            line2 = f.readline()
+            line3 = f.readline()
+            line4 = f.readline().strip()
+            if not line1:
+                break
+            # rows correspond to nucleotides in data_title
+            # and are denoted by r
+            for r in range(101):
+                q = phred(line4[r])
+                data[r]+=q
+        data = data/n
+        return data       
+
+n = 4000000
+#dir='/mnt/c/Users/effyg/gdrive2/Bi622/De-multiplex/'
+dir='/home/agill/'
+file=files[1]
+print(file)
+array = bp_means(file, n)
+print('array created')
+numpy.savetxt("R2.csv", array, delimiter=",")
+
+
